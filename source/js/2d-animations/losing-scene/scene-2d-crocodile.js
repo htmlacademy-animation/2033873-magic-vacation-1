2e9b3537b2c4035b4ae589b4a19a85833f4a542d
M.Studitskiy <cayman2312@gmail.com>
2022-07-10 15:04:39 +0500

    добавил падающие объекты

--- source/js/2d-animations/losing-scene/scene-2d-crocodile.js
+++ source/js/2d-animations/losing-scene/scene-2d-crocodile.js
@@ -1,16 +1,16 @@
 import Scene2D from "../Animation/scene-2d";
 import Animation from "../Animation/Animation";
-import {  easeInOutSine } from "../../helpers/easing";
+import { easeInCubic, easeInOutSine, easeOutCubic } from "../../helpers/easing";
 
 const IMAGES_URLS = Object.freeze({
   key: "key.png",
   // crocodile: "crocodile.png",
   // drop: "drop.png",
-  // flamingo: "flamingo.png",
-  // leaf: "leaf.png",
-  // saturn: "saturn.png",
-  // snowFlake: "snowFlake.png",
-  // watermelon: "watermelon.png",
+  flamingo: "flamingo.png",
+  leaf: "leaf.png",
+  saturn: "saturn.png",
+  snowFlake: "snowFlake.png",
+  watermelon: "watermelon.png",
 });
 
 const OBJECTS = Object.freeze({
@@ -18,9 +18,50 @@ const OBJECTS = Object.freeze({
     imageId: "key",
     x: 50,
     y: 58,
-    size: 22,
+    size: 18,
     opacity: 0,
     transforms: { scaleX: 0.8, scaleY: 0.8 },
+  },
+  flamingo: {
+    imageId: "flamingo",
+    x: 50,
+    y: 58,
+    size: 16,
+    opacity: 0,
+    transforms: {},
+  },
+  watermelon: {
+    imageId: "watermelon",
+    x: 50,
+    y: 58,
+    size: 16,
+    opacity: 0,
+    transforms: {},
+  },
+  snowFlake: {
+    imageId: "snowFlake",
+    x: 50,
+    y: 58,
+    size: 12,
+    opacity: 0,
+    transforms: {},
+  },
+  saturn: {
+    imageId: "saturn",
+    x: 50,
+    y: 58,
+    size: 16,
+    opacity: 0,
+    transforms: {},
+  },
+
+  leaf: {
+    imageId: "leaf",
+    x: 50,
+    y: 58,
+    size: 20,
+    opacity: 0,
+    transforms: {},
   },
 });
 
@@ -56,6 +97,11 @@ export default class Scene2dCrocodile extends Scene2D {
     );
 
     this.initKeyAnimations();
+    this.initFlamingoAnimations();
+    this.initWatermelonAnimation();
+    this.initSnowFlakeAnimation();
+    this.initSaturnAnimation();
+    this.initLeafAnimation();
   }
 
   initKeyAnimations() {
@@ -69,6 +115,126 @@ export default class Scene2dCrocodile extends Scene2D {
         duration: 187,
         delay: 0,
         easing: easeInOutSine,
+      })
+    );
+  }
+
+  initFlamingoAnimations() {
+    this.animations.push(
+      new Animation({
+        func: (progress) => {
+          this.objects.flamingo.opacity = progress;
+          this.objects.flamingo.transforms.translateX = -25 * progress;
+          this.objects.flamingo.transforms.translateY = -5 * progress;
+          this.objects.flamingo.transforms.rotate = (1 - progress) * 60;
+        },
+        duration: 617,
+        delay: 100,
+        easing: easeOutCubic,
+      }),
+      new Animation({
+        func: (progress) => {
+          this.objects.flamingo.transforms.translateY = -5 + 80 * progress;
+        },
+        duration: 583,
+        delay: 717,
+        easing: easeInCubic,
+      })
+    );
+  }
+
+  initWatermelonAnimation() {
+    this.animations.push(
+      new Animation({
+        func: (progress) => {
+          this.objects.watermelon.opacity = progress;
+          this.objects.watermelon.transforms.translateX = -40 * progress;
+          this.objects.watermelon.transforms.translateY = 20 * progress;
+          this.objects.watermelon.transforms.rotate = (1 - progress) * 60;
+        },
+        duration: 617,
+        delay: 100,
+        easing: easeOutCubic,
+      }),
+      new Animation({
+        func: (progress) => {
+          this.objects.watermelon.transforms.translateY = 20 + 80 * progress;
+        },
+        duration: 583,
+        delay: 717,
+        easing: easeInCubic,
+      })
+    );
+  }
+
+  initSnowFlakeAnimation() {
+    this.animations.push(
+      new Animation({
+        func: (progress) => {
+          this.objects.snowFlake.opacity = progress;
+          this.objects.snowFlake.transforms.translateX = 22 * progress;
+          this.objects.snowFlake.transforms.translateY = 6 * progress;
+          this.objects.snowFlake.transforms.rotate = (1 - progress) * 60;
+        },
+        duration: 617,
+        delay: 100,
+        easing: easeOutCubic,
+      }),
+      new Animation({
+        func: (progress) => {
+          this.objects.snowFlake.transforms.translateY = 6 + 80 * progress;
+        },
+        duration: 583,
+        delay: 717,
+        easing: easeInCubic,
+      })
+    );
+  }
+
+  initSaturnAnimation() {
+    this.animations.push(
+      new Animation({
+        func: (progress) => {
+          this.objects.saturn.opacity = progress;
+          this.objects.saturn.transforms.translateX = 36 * progress;
+          this.objects.saturn.transforms.translateY = 22 * progress;
+          this.objects.saturn.transforms.rotate = (1 - progress) * 60;
+        },
+        duration: 617,
+        delay: 100,
+        easing: easeOutCubic,
+      }),
+      new Animation({
+        func: (progress) => {
+          this.objects.saturn.transforms.translateY = 22 + 80 * progress;
+        },
+        duration: 583,
+        delay: 717,
+        easing: easeInCubic,
+      })
+    );
+  }
+
+  initLeafAnimation() {
+    this.animations.push(
+      new Animation({
+        func: (progress) => {
+          this.objects.leaf.opacity = progress;
+          this.objects.leaf.transforms.translateX = 40 * progress;
+          this.objects.leaf.transforms.translateY = -10 * progress;
+          this.objects.leaf.transforms.rotate = (1 - progress) * 60;
+        },
+        duration: 617,
+        delay: 100,
+        easing: easeOutCubic,
+      }),
+      new Animation({
+        func: (progress) => {
+          this.objects.leaf.transforms.translateY = -10 + 80 * progress;
+        },
+        duration: 583,
+        delay: 717,
+        easing: easeInCubic,
       })
     );
   }
