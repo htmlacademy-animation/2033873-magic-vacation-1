f14c53a9d1621aa63c776ef03e41f6b6d78110a2
m.studitskiy <cayman2312@gmail.com>
2022-10-10 12:46:03 +0500

    поправил metalness

--- source/js/3d-animations/creators/MaterialCreator.js
+++ source/js/3d-animations/creators/MaterialCreator.js
@@ -179,7 +179,7 @@ MaterialCreator.Colors = {
 MaterialCreator.Config = {
   SoftMaterial: {
     roughness: 0.9,
-    metalness: 0.4,
+    metalness: 0.0,
     name: "SoftMaterial",
   },
   BasicMaterial: {
