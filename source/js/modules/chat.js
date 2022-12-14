202643b4f6f13c99db540b9605dee4856d0ef5a6
m.studitskiy <cayman2312@gmail.com>
2022-12-14 12:04:28 +0600

    добавил плавное появление сообщений в чате

--- source/js/modules/chat.js
+++ source/js/modules/chat.js
@@ -3,63 +3,70 @@ export default () => {
   let messageField = document.getElementById(`message-field`);
   let messageList = document.getElementById(`messages`);
   let chatBlock = document.querySelector(`.js-chat`);
+  let chatList = chatBlock.querySelector(`.chat__list`);
 
   messageForm.addEventListener(`submit`, function (e) {
     e.preventDefault();
 
-    let scrollToBottom = function () {
-      if (messageList.scrollHeight > chatBlock.offsetHeight) {
-        chatBlock.scrollTop = messageList.scrollHeight;
-      }
-    };
+    function scrollToBottom() {
+      chatList.scrollTo({
+        top: chatList.scrollHeight,
+        behavior: "smooth",
+      });
+    }
 
-    let getAnswer = function () {
-      setTimeout(function () {
-        let answerEl = document.createElement(`li`);
-        let placeholder = document.createElement(`div`);
-        let textEl = document.createElement(`p`);
-        placeholder.classList.add(`chat__placeholder`);
-        for (let i = 0; i < 3; i++) {
-          let dot = document.createElement(`span`);
-          placeholder.appendChild(dot);
-        }
-        answerEl.appendChild(placeholder);
-        answerEl.classList.add(`chat__message`);
-        answerEl.classList.add(`chat__message--incoming`);
-        answerEl.classList.add(`chat__message--last`);
-        let answer = Math.floor(Math.random() * 2);
-        let answerText;
-
-        if (answer) {
-          answerText = `Да`;
-        } else {
-          answerText = `Нет`;
-        }
-
-        textEl.innerText = answerText;
-        textEl.classList.add(`hidden`);
-        answerEl.appendChild(textEl);
-        messageList.appendChild(answerEl);
-        scrollToBottom();
-
+    function getAnswer() {
+      return new Promise((resolve) => {
         setTimeout(function () {
-          let lastMessage = document.querySelector(`.chat__message--last`);
-          if (lastMessage) {
-            let lastMessagePlaceholder =
-              lastMessage.querySelector(`.chat__placeholder`);
-            let lastMessageText = lastMessage.querySelector(`p`);
-            lastMessagePlaceholder.classList.add(`chat__placeholder--hidden`);
-            setTimeout(function () {
-              lastMessagePlaceholder.remove();
-            }, 400);
-            lastMessageText.classList.remove(`hidden`);
-            lastMessage.classList.remove(`chat__message--last`);
+          let answerEl = document.createElement(`li`);
+          let placeholder = document.createElement(`div`);
+          let textEl = document.createElement(`p`);
+          placeholder.classList.add(`chat__placeholder`);
+          for (let i = 0; i < 3; i++) {
+            let dot = document.createElement(`span`);
+            placeholder.appendChild(dot);
           }
-        }, 700);
-      }, 700);
-    };
+          answerEl.appendChild(placeholder);
+          answerEl.classList.add(`chat__message`);
+          answerEl.classList.add(`chat__message--incoming`);
+          answerEl.classList.add(`chat__message--last`);
+          let answer = Math.floor(Math.random() * 2);
+          let answerText;
 
-    let postQuestion = function () {
+          if (answer) {
+            answerText = `Да`;
+          } else {
+            answerText = `Нет`;
+          }
+
+          textEl.innerText = answerText;
+          textEl.classList.add(`hidden`);
+          answerEl.appendChild(textEl);
+          messageList.appendChild(answerEl);
+          scrollToBottom();
+
+          setTimeout(function () {
+            let lastMessage = document.querySelector(`.chat__message--last`);
+            if (lastMessage) {
+              let lastMessagePlaceholder =
+                lastMessage.querySelector(`.chat__placeholder`);
+              let lastMessageText = lastMessage.querySelector(`p`);
+              lastMessagePlaceholder.classList.add(`chat__placeholder--hidden`);
+              // eslint-disable-next-line max-nested-callbacks
+              setTimeout(() => {
+                lastMessagePlaceholder.remove();
+              }, 400);
+              lastMessageText.classList.remove(`hidden`);
+              lastMessage.classList.remove(`chat__message--last`);
+            }
+
+            resolve();
+          }, 700);
+        }, 700);
+      });
+    }
+
+    let postQuestion = async function () {
       if (messageField.value) {
         let messageEl = document.createElement(`li`);
         messageEl.classList.add(`chat__message`);
@@ -71,9 +78,10 @@ export default () => {
         messageList.appendChild(messageEl);
         messageField.value = ``;
         messageField.setAttribute(`disabled`, `true`);
+
         scrollToBottom();
 
-        getAnswer();
+        await getAnswer();
 
         messageField.removeAttribute(`disabled`);
         messageField.focus();
